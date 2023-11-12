8bbb40330f4fd79c6878f67a6384fad3bbc44568
Thingk0 <audtjd9080@naver.com>
2023-11-12 20:55:04 +0900

    :tada: Motif:모티브 - Frontend Project Init.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+@font-face {
+  font-family: 'BM Jua';
+  src: url('assets/fonts/BMJUA.otf') format('truetype');
+  font-weight: auto;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { adorableCSS } from 'adorable-css/vite'
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [adorableCSS(), vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
