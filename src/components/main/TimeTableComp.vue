c2812269efef23520ea629526d73f20b3b9eade7
Thingk0 <audtjd9080@naver.com>
2023-11-20 10:23:50 +0900

    :lipstick: 타임테이블 높이 동적으로 조절 및 Icon y값 중앙정렬.

--- src/components/main/TimeTableComp.vue
+++ src/components/main/TimeTableComp.vue
@@ -3,12 +3,12 @@
 <template>
   <div class="z(1) layer(bottom:25) pack">
     <div
-      class="relative bg(white) w(90%) h(40) r(25) box-shadow(0/0/5/#A4A4A4) hover:w(92%)+h(4.1vh) transition(.5s)"
+      class="relative bg(white) w(90%) h(4vh) r(25) box-shadow(0/0/5/#A4A4A4) hover:w(92%)+h(4.1vh) transition(.5s)"
     >
       <img
         src="@/assets/images/icon/timetable_clock._icon.svg"
         alt="search-icon"
-        class="w(20) h(20) absolute x(20) y(10)"
+        class="w(20) h(20) absolute x(20) y(center)"
       />
     </div>
   </div>
